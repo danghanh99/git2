0cfece1207dffa010764a4dec65967ad6c66c024
hanh <ldhanh1999@gmail.com>
2019-12-01 10:23:00 +0700

    add s6.rb

---
+++ s6.rb
@@ -0,0 +1,11 @@
+# Enter your code here
+def strike(text)
+return "<strike>#{text}</strike>"
+end
+
+def mask_article(s1,texts)
+ texts.each do |text|
+     s1.gsub!(text, strike(text))    
+ end
+  s1
+end
