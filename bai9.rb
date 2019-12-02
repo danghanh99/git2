ff575037883923ebe9ae63dccdd0f704db97da2e
hanh <ldhanh1999@gmail.com>
2019-12-02 15:49:25 +0700

    edit bai9.rb

--- bai9.rb
+++ bai9.rb
@@ -1,17 +1,14 @@
-
 def identify_class(obj)
-    # write your case control structure here
-    case obj
+  case obj
 when Hacker
-   puts "It's a Hacker!"
+  puts "It's a Hacker!"
 when Submission 
-        puts "It's a Submission!"
+  puts "It's a Submission!"
 when TestCase
-        puts "It's a TestCase!"
+  puts "It's a TestCase!"
 when Contest
-        puts "It's a Contest!"
+  puts "It's a Contest!"
 else
-   puts "It's an unknown model" 
+  puts "It's an unknown model" 
 end
 end
-
