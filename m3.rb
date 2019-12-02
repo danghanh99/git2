7487ed183c13d623782182847f1c9c50affb5d4d
hanh <ldhanh1999@gmail.com>
2019-12-02 17:46:27 +0700

    fix join

--- m3.rb
+++ m3.rb
@@ -1,11 +1,10 @@
-# Your code here
 def full_name(first, *midle,last)
-    fullname = ''
-    fullname+=first
-    midle.each do |name|
-        fullname += " "
-        fullname += name
-    end  
+  fullname = ''
+  fullname+=first
+  midle.each do |name|
     fullname += " "
-    fullname += last
+    fullname += name
+  end  
+  fullname += " "
+  fullname += last
 end
