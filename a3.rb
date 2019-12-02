dd827510b7b044e63c136693d548c8cd515cbcef
hanh <ldhanh1999@gmail.com>
2019-12-02 18:01:18 +0700

    delete return

--- a3.rb
+++ a3.rb
@@ -1,17 +1,17 @@
 def neg_pos(arr, index)
   # return the element of the array at the position `index` from the end of the list
   # Clue : arr[-index]
-  return arr[-index]
+  arr[-index]
 end
 def first_element(arr)
   # return the first element of the array
   # arr.first
-  return arr.first
+  arr.first
 end
 def last_element(arr)
   # return the last element of the array
   # arr.last
-  return arr.last
+  arr.last
 end
 def first_n(arr, n)
   # return the first n elements of the array
@@ -19,5 +19,5 @@ def first_n(arr, n)
 end
 def drop_n(arr, n)
   # drop the first n elements of the array and return the rest
-  return arr.drop(n)
+  arr.drop(n)
 end
