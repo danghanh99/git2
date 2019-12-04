e87205b09b18077999b057f24294f9bafb850c11
hanh <ldhanh1999@gmail.com>
2019-12-04 20:50:16 +0700

    delete return not useful

--- s4.rb
+++ s4.rb
@@ -1,9 +1,7 @@
-# Your code here
 def count_multibyte_char(s1)
  count =0
-    s1.each_char do |kytu|
-        count +=1 if kytu.bytesize > 1
-    end
-    return count
-
+  s1.each_char do |kytu|
+    count +=1 if kytu.bytesize > 1
+  end
+  count
 end
